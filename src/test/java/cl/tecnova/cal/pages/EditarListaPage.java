40b43bd140e69bc2c4bb6a5b1f6e65b5ab007f4b
Jose Miguel Lopez <josemiguellopez@outlook.cl>
2019-08-18 14:38:41 -0400

    Edicion Descripcion
    Se modica caso de prueba editar descripcion

--- src/test/java/cl/tecnova/cal/pages/EditarListaPage.java
+++ src/test/java/cl/tecnova/cal/pages/EditarListaPage.java
@@ -29,7 +29,7 @@ public class EditarListaPage {
 		private  By cerraretiqueta;
 		private By vencimiento;
 		private By campofecha;
-//		private By campohora;
+		private By campohora;
 		private By guardar;
 		private By agregarcomentario;
 		private By adjunto;
@@ -55,7 +55,7 @@ public class EditarListaPage {
 					this.cerraretiqueta = By.xpath("//a[@class='pop-over-header-close-btn icon-sm icon-close']");
 					this.vencimiento = By.xpath("//a[contains(.,'Vencimiento')]");
 					this.campofecha = By.xpath("//input[@tabindex='101']");
-//					this.campohora = By.xpath("//input[@tabindex='102']");
+					this.campohora = By.xpath("//input[@placeholder='Introducir la hora']");
 					this.guardar = By.xpath("//input[@tabindex='103']");
 					this.agregarcomentario = By.xpath("//textarea[@placeholder='Escriba un comentario...']");
 					this.adjunto = By.xpath("//a[@title='Adjunto']");
@@ -70,43 +70,66 @@ public class EditarListaPage {
 					
 					
 							}
-		public void EditarLista(String subDir){
-			
+				
+				
+		public void TarjetaPruebaAutomatizada(String subDir) {
 			Helper.waitSeconds(3);
 			driver.findElement(clickpruebaautomatizada).click();
 			Helper.waitSeconds(2);
+			Helper.addEvidence(TAKE_SS, driver, test, "Pantalla de Ingreso Prueba Automatizada", subDir, "IngresoPruebaAutomatizada");
+
+					}
+		public void Descripcion(String subDir) {
 			driver.findElement(clickdescripcion).click();
 			Helper.waitSeconds(1);
 			driver.findElement(descripcion).sendKeys("Hola soy una descripcion automatizada");
-			Helper.waitSeconds(2);
+			Helper.addEvidence(TAKE_SS, driver, test, "Pantalla de Agregar Descricpion", subDir, "AgregarDescripcion");
+
+			Helper.waitSeconds(2);			
+		}
+		public void Etiqueta(String subDir) {
 			driver.findElement(etiqueta).click();
 			Helper.waitSeconds(1);
 			driver.findElement(coloretiqueta).click();
 			Helper.waitSeconds(1);
+			Helper.addEvidence(TAKE_SS, driver, test, "Pantalla de Agregar Etiqueta", subDir, "AgregarEtiqueta");
+
 			driver.findElement(cerraretiqueta).click();
-			Helper.waitSeconds(1);
-			// hasta aqui la vaina va bien, no cambies nada, cualquier error es cosa de chrome !
+			Helper.waitSeconds(1);			
+		}
+		
+		public void Vencimiento(String subDir) {
+			
 			driver.findElement(vencimiento).click();
 			Helper.waitSeconds(1);
-			driver.findElement(campofecha).sendKeys("31/05/2019");
+			driver.findElement(campofecha).sendKeys("31/08/2019");
 			Helper.waitSeconds(1);
-//			driver.findElement(campohora).sendKeys("12:55");
-//			Select sel=new Select(campohora);
-//		    sel.selectByVisibleText("12:55"); //This will select the 'Option' from 'Select' List who's visibility text is "Cycle". 
-			
-			
+			driver.findElement(campohora).sendKeys("12:55");
 			Helper.waitSeconds(1);
+			Helper.addEvidence(TAKE_SS, driver, test, "Pantalla de Agregar Vencimiento", subDir, "AgregarVencimiento");
+
 			driver.findElement(guardar).click();
 			
+		}
+		
+		public void ArchivoAdjunto(String subDir) {
 			Helper.waitSeconds(1);
-			
 			driver.findElement(adjunto).click();
 			driver.findElement(txtadjunto).sendKeys("https://avatarfiles.alphacoders.com/991/thumb-99177.jpg");
+			Helper.addEvidence(TAKE_SS, driver, test, "Pantalla de Agregar Archivo Adjunto", subDir, "AgregarArchivoAdjunto");
+
 			driver.findElement(adjuntar).click();
+					
+		}
+		
+							
+		public void Comentarios(String subDir){
+			
+			
 			driver.findElement(agregarcomentario).sendKeys("Hola, Soy un comentario automatizado ;)");
 			Helper.waitSeconds(1);
 			driver.findElement(guardarprueba).click();
-			Helper.addEvidence(TAKE_SS, driver, test, "Pantalla de Editar Tarjeta", subDir, "EditarTarjeta");
+			Helper.addEvidence(TAKE_SS, driver, test, "Pantalla de Editar Tarjeta", subDir, "EditarTarjetaCoemntarios");
 			Helper.waitSeconds(1);
 			driver.findElement(cerrar).click();
 
