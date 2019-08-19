0c37b76136e161b6d32362267e7ebb51e99b869d
Jose Miguel Lopez <josemiguellopez@outlook.cl>
2019-08-19 14:40:18 -0400

    Ordendando Code
    Ordendando Code

--- src/test/java/cl/tecnova/cal/pages/CrearTableroPage.java
+++ src/test/java/cl/tecnova/cal/pages/CrearTableroPage.java
@@ -25,7 +25,6 @@ public class CrearTableroPage {
 	private By agregartitulo;
 	private By creartablero;
 	
-	
 	//Atributos Eliminar Tablero
 	private By btnmas;
 	private By cerrar;
@@ -36,8 +35,6 @@ public class CrearTableroPage {
 	//Atributos_Asserts
 	private By validaciontablerocreado;
 
-	
-	
 	//Construcctor
 	public CrearTableroPage(WebDriver driver, ExtentTest test, Boolean TAKE_SS) {
 				
@@ -46,18 +43,13 @@ public class CrearTableroPage {
 	this.TAKE_SS = TAKE_SS;
 	this.clickcrearuntablero = By.xpath("//div[@class='board-tile mod-add']");
 	this.agregartitulo = By.xpath("//*[@id=\"chrome-container\"]/div[3]/div/div/div/form/div/div/div[1]/input");
-	this.creartablero = By.xpath("//*[@id=\"chrome-container\"]/div[3]/div/div/div/form/button");
-	
-	
-	//Constructor Eliminar Tablero
-	
+	this.creartablero = By.xpath("//*[@id=\"chrome-container\"]/div[3]/div/div/div/form/button");	
+	//Constructor Eliminar Tablero	
 	this.btnmas = By.xpath("//a[contains(.,'Más')]");
 	this.cerrar = By.xpath("//a[contains(.,'Cerrar tablero...')]");
 	this.cerrar2 = By.xpath("//input[@value='Cerrar']");
 	this.cerrar3 = By.xpath("//a[contains(.,'Eliminar el tablero de forma permanente...')]");
-	this.cerrar4 = By.xpath("//input[@value='Eliminar']");
-	
-	
+	this.cerrar4 = By.xpath("//input[@value='Eliminar']");	
 	//Constructor_Assert
 	this.validaciontablerocreado = By.xpath("//span[contains(.,'Tablero de Jose de Prueba')]");
 
@@ -84,26 +76,14 @@ public class CrearTableroPage {
 		Helper.waitSeconds(1);
 		driver.findElement(cerrar2).click();
 		Helper.addEvidence(TAKE_SS, driver, test, "Pantalla de Cerrar Tablero", subDir, "CerrarTablero");
-		Helper.waitSeconds(1);
-		
+		Helper.waitSeconds(1);		
 		driver.findElement(cerrar3).click();
 		Helper.waitSeconds(1);
 		driver.findElement(cerrar4).click();
 		Helper.waitSeconds(1);
-
 		Helper.addEvidence(TAKE_SS, driver, test, "Pantalla de Tablero Eliminado", subDir, "TableroEliminado");
-
-
-
 	}
-
-	
-	
-	
-	
-	
-	
-		public void assertCrearTableroValidacion() { //metodo para validar ingreso correcto      //#Paso 5 	
+	public void assertCrearTableroValidacion() { //metodo para validar ingreso correcto      //#Paso 5 	
 		Assert.assertTrue(driver.findElement(validaciontablerocreado).getText().equals("Tablero de Jose de Prueba"));
 
 		}
